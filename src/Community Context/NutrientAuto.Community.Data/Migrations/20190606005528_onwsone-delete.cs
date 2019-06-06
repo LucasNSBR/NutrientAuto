47df1cacb432c04471e2c6d1f4157c90ac4b45e3
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2019-06-05 22:07:39 -0300

    Run migraitons

---
+++ src/Community Context/NutrientAuto.Community.Data/Migrations/20190606005528_onwsone-delete.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NutrientAuto.Community.Data.Migrations
+{
+    public partial class onwsonedelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Comments_ReplyTo",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Comments_ReplyTo",
+                table: "Comments",
+                column: "ReplyTo",
+                principalTable: "Comments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Comments_ReplyTo",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Comments_ReplyTo",
+                table: "Comments",
+                column: "ReplyTo",
+                principalTable: "Comments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
