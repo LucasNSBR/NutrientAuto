b0237299ac297d11d92fe4878e16b14ac5abf443
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2019-06-06 19:06:47 -0300

    Create PostFactory

---
+++ src/Community Context/NutrientAuto.Community.Domain/Factories/PostAggregate/PostFactory.cs
@@ -0,0 +1,43 @@
+﻿using NutrientAuto.Community.Domain.Aggregates.PostAggregate;
+using System;
+
+namespace NutrientAuto.Community.Domain.Factories.PostAggregate
+{
+    public class PostFactory : IPostFactory
+    {
+        public Post CreateGoalRegisteredPost(Guid profileId, Guid goalId)
+        {
+            EntityReference entityReference = EntityReference.Goal(goalId);
+
+            return new Post(profileId, "Novo objetivo definido", $"Você pode ver mais detalhes no meu registro de objetivos.", entityReference);
+        }
+
+        public Post CreateGoalCompletedPost(Guid profileId, Guid measureId)
+        {
+            EntityReference entityReference = EntityReference.Measure(measureId);
+
+            return new Post(profileId, "Objetivo completado", $"Eu completei um objetivo. Veja mais detalhes no meu registro de objetivos.", entityReference);
+        }
+
+        public Post CreateMeasureRegisteredPost(Guid profileId, Guid measureId)
+        {
+            EntityReference entityReference = EntityReference.Measure(measureId);
+
+            return new Post(profileId, "Nova medição realizada", $"Você pode ver mais detalhes no meu registro de medições.", entityReference);
+        }
+
+        public Post CreateDietRegisteredPost(Guid profileId, Guid dietId)
+        {
+            EntityReference entityReference = EntityReference.Diet(dietId);
+
+            return new Post(profileId, "Nova Dieta iniciada", $"Eu iniciei uma nova dieta. Veja mais detalhes no meu registro de dietas.", entityReference);
+        }
+
+        public Post CreateProfileUpdatedPost(Guid profileId)
+        {
+            EntityReference entityReference = EntityReference.Profile(profileId);
+
+            return new Post(profileId, "Perfil atualizado", $"Atualizei as informações do meu perfil. Veja mais detalhes no sumário do meu perfil.", entityReference);
+        }
+    }
+}
