33224874803e1e0d61bd97815d023d07b154fe63
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2019-06-07 00:22:38 -0300

    Add Image and MeasureLine to Automapper profile

--- src/CrossCutting/NutrientAuto.CrossCutting.Mapping/Profiles/Community/CommunityMappingProfile.cs
+++ src/CrossCutting/NutrientAuto.CrossCutting.Mapping/Profiles/Community/CommunityMappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using NutrientAuto.Community.Domain.Aggregates.MeasureAggregate;
 using NutrientAuto.Community.Domain.Aggregates.SeedWork;
 using NutrientAuto.Community.Domain.Commands.SeedWork;
+using NutrientAuto.Shared.ValueObjects;
 
 namespace NutrientAuto.CrossCutting.Mapping.Profiles.Community
 {
@@ -10,6 +12,8 @@ namespace NutrientAuto.CrossCutting.Mapping.Profiles.Community
         {
             CreateMap<MacronutrientTableDto, MacronutrientTable>();
             CreateMap<MicronutrientTableDto, MicronutrientTable>();
+            CreateMap<ImageDto, Image>();
+            CreateMap<MeasureLineDto, MeasureLine>();
         }
     }
 }
