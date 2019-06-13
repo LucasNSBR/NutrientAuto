b28d982ac04fadcb62a82393bfa481fcc2715b44
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2019-06-13 00:51:53 -0300

    Add PostLikeReadModel

---
+++ src/Community Context/NutrientAuto.Community.Domain/ReadModels/PostAggregate/PostLikeReadModel.cs
@@ -0,0 +1,12 @@
+﻿using NutrientAuto.Shared.ValueObjects;
+using System;
+
+namespace NutrientAuto.Community.Domain.ReadModels.PostAggregate
+{
+    public class PostLikeReadModel
+    {
+        public Guid ProfileId { get; set; }
+        public Image ProfileAvatarImage { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
