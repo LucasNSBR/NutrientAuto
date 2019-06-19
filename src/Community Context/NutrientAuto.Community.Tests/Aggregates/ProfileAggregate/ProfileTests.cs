a83475b5154db4768fe3f9a44c831fff9b44e132
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2019-06-18 21:55:26 -0300

    Finish Profile aggregate tests

--- src/Community Context/NutrientAuto.Community.Tests/Aggregates/ProfileAggregate/ProfileTests.cs
+++ src/Community Context/NutrientAuto.Community.Tests/Aggregates/ProfileAggregate/ProfileTests.cs
@@ -2,6 +2,7 @@
 using NutrientAuto.Community.Domain.Aggregates.ProfileAggregate;
 using NutrientAuto.Shared.ValueObjects;
 using System;
+using System.Linq;
 
 namespace NutrientAuto.Community.Tests.Aggregates.ProfileAggregate
 {
@@ -21,7 +22,7 @@ namespace NutrientAuto.Community.Tests.Aggregates.ProfileAggregate
             return avatarImage;
         }
 
-        private Profile GetProfile()
+        private Profile GetNewProfile()
         {
             Guid profileId = Guid.NewGuid();
             Profile profile = new Profile(profileId, Genre.Male, GetAvatarImage(), "Lucas Pereira Campos", "lucasnsbr", GetEmailAddress(), DateTime.Now.AddYears(-21));
@@ -33,7 +34,7 @@ namespace NutrientAuto.Community.Tests.Aggregates.ProfileAggregate
         [TestMethod]
         public void ShouldSetInitialVariables()
         {
-            Profile profile = GetProfile();
+            Profile profile = GetNewProfile();
 
             Assert.AreEqual(Genre.Male, profile.Genre);
             Assert.AreEqual(GetAvatarImage(), profile.AvatarImage);
@@ -46,7 +47,7 @@ namespace NutrientAuto.Community.Tests.Aggregates.ProfileAggregate
         [TestMethod]
         public void ShouldInitializeDefaultPrivacySettings()
         {
-            Profile profile = GetProfile();
+            Profile profile = GetNewProfile();
 
             Assert.IsNotNull(profile.Settings);
             Assert.AreEqual(PrivacyType.Private, profile.Settings.PrivacyType);
@@ -57,7 +58,7 @@ namespace NutrientAuto.Community.Tests.Aggregates.ProfileAggregate
         [TestMethod]
         public void ShouldUpdateProfileAggregate()
         {
-            Profile profile = GetProfile();
+            Profile profile = GetNewProfile();
 
             profile.Update(Genre.Male, "Pedro", "Pedronsbr", DateTime.Now.AddYears(-50), "Colocando uma nova bio");
 
@@ -66,6 +67,96 @@ namespace NutrientAuto.Community.Tests.Aggregates.ProfileAggregate
             Assert.AreEqual("Pedronsbr", profile.Username);
             Assert.AreEqual(DateTime.Now.AddYears(-50).Year, profile.BirthDate.Year);
             Assert.AreEqual("Colocando uma nova bio", profile.Bio);
+        }
+
+        [TestMethod]
+        public void ShouldChangeAvatarImage()
+        {
+            Profile profile = GetNewProfile();
+            Image newAvatarImage = new Image("s3.aws.storage/150", "IMG015100");
+
+            profile.SetAvatarImage(newAvatarImage);
+
+            Assert.AreEqual("s3.aws.storage/150", profile.AvatarImage.UrlPath);
+            Assert.AreEqual("IMG015100", profile.AvatarImage.ImageName);
+        }
+
+        [TestMethod]
+        public void ShouldChangeProfileSettings()
+        {
+            Profile profile = GetNewProfile();
+            ProfileSettings settings = new ProfileSettings(PrivacyType.Public);
+
+            profile.ChangeSettings(settings);
+
+            Assert.IsNotNull(profile.Settings);
+            Assert.AreEqual(PrivacyType.Public, profile.Settings.PrivacyType);
+        }
+        #endregion
+
+        #region Friends 
+        [TestMethod]
+        public void ShouldAddFriend()
+        {
+            Profile profile = GetNewProfile();
+            Profile profileTwo = GetNewProfile();
+
+            profile.AddFriend(profileTwo);
+
+            Assert.IsTrue(profile.IsValid);
+            Assert.AreEqual(1, profile.Friends.Count);
+        }
+        
+        [TestMethod]
+        public void ShouldFailToAddFriendSameProfile()
+        {
+            Profile profile = GetNewProfile();
+            
+            profile.AddFriend(profile);
+
+            Assert.IsFalse(profile.IsValid);
+            Assert.AreEqual("Você não pode adicionar a si mesmo.", profile.GetNotifications().FirstOrDefault().Description);
+        }
+
+        [TestMethod]
+        public void ShouldFailToAddFriendAlreadyFriends()
+        {
+            Profile profile = GetNewProfile();
+            Profile profileTwo = GetNewProfile();
+
+            profile.AddFriend(profileTwo);
+            profile.AddFriend(profileTwo);
+
+            Assert.IsFalse(profile.IsValid);
+            Assert.AreEqual("Você e esse usuário já são amigos.", profile.GetNotifications().FirstOrDefault().Description);
+        }
+
+        [TestMethod]
+        public void ShouldCancelFriendship()
+        {
+            Profile profile = GetNewProfile();
+            Profile profileTwo = GetNewProfile();
+
+            profile.AddFriend(profileTwo);
+
+            Assert.AreEqual(1, profile.Friends.Count);
+
+            profile.RemoveFriend(profileTwo);
+
+            Assert.IsTrue(profile.IsValid);
+            Assert.AreEqual(0, profile.Friends.Count);
+        }
+
+        [TestMethod]
+        public void ShouldFailToCancelFriendshipNotFriends()
+        {
+            Profile profile = GetNewProfile();
+            Profile profileTwo = GetNewProfile();
+
+            profile.RemoveFriend(profileTwo);
+
+            Assert.IsFalse(profile.IsValid);
+            Assert.AreEqual("Você e esse usuário ainda não são amigos.", profile.GetNotifications().FirstOrDefault().Description);
         }
         #endregion
     }
